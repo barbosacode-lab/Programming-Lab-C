6144de6dc6aec281b0f68f6ca53546a225dc5e4e
Leandro Barbosa <leandro.barbosamkt@gmail.com>
2021-12-04 12:12:06 -0300

    feat[intro-memory]: concepts about endianness and cpu architecture

---
+++ intro-memory-usage/endianness.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int a = 0x12345678;
+    unsigned char *c = (unsigned char *)(&a);
+    if (*c == 0x78) {
+        printf("little-endian\n");
+    } else {
+        printf("big-endian\n");
+    }
+    return 0;
+}
